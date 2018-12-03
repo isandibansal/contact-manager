b35e820ba366d1b1428e6e4dae56bd74c8c0de01
isandibansal <iamsandibansal@outlook.com>
2018-12-03 22:28:32 +0530

    Working with props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Contact from './components/Contact';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App JS</h1>
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact name="john Doe" email="jdoe@gmail.com" phone="555-6668-999" />
+        <Contact name="karen" email="karen@gmail.com" phone="555-7777-999" />
       </div>
     );
   }
