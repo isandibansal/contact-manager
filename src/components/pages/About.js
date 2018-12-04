ca850e2b544c81a278641d3a7f79a9c21499be25
isandibansal <iamsandibansal@outlook.com>
2018-12-04 15:49:41 +0530

    react-router-dom added and Routes created

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">Simple contact Manager App version 1.0</p>
+    </div>
+  );
+};
+
+export default About;
