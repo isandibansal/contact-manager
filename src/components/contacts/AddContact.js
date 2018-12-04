05115ce7523eb1f03de31035b9ef37c1bdc3ec1c
isandibansal <iamsandibansal@outlook.com>
2018-12-04 14:25:34 +0530

    TextInputGroup component

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import uuid from 'uuid';
+import TextInputGroup from '../layout/TextInputGroup';
+
 class AddContact extends Component {
   state = {
     name: '',
@@ -41,39 +43,27 @@ class AddContact extends Component {
               <div className="card-header">Add Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="Enter Name"
-                      className="form-control form-control-lg"
-                      value={name}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Enter Email.."
-                      className="form-control form-control-lg"
-                      value={email}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                      type="text"
-                      name="phone"
-                      placeholder="Enter Phone..."
-                      className="form-control form-control-lg"
-                      value={phone}
-                      onChange={this.onChange}
-                    />
-                  </div>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name.."
+                    value={name}
+                    onChange={this.onChange}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    placeholder="Enter Email.."
+                    value={email}
+                    onChange={this.onChange}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter Phone.."
+                    value={phone}
+                    onChange={this.onChange}
+                  />
                   <input
                     type="submit"
                     value="Add Contact"
---
+++ src/components/layout/TextInputGroup.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextInputGroup = props => {
+  const { label, name, value, placeholder, type, onChange } = props;
+  return (
+    <div className="form-group">
+      <label htmlFor="name">{label}</label>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        className="form-control form-control-lg"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+TextInputGroup.defaultProps = {
+  type: 'text'
+};
+
+TextInputGroup.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+export default TextInputGroup;
